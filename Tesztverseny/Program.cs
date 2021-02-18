b1d698766b87e8ed64c6f8e9567c39adc0418340
borossandor27 <boros.sanyi@gmail.com>
2021-02-18 10:05:51 +0100

    Add project files.

---
+++ Tesztverseny/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Tesztverseny
+{
+    class Program
+    {
+        static string joValasz = null;
+        static List<Teszt> tesztek = new List<Teszt>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. feladat: 'valaszok.txt' beolvasása ...");
+            Beolvas("valaszok.txt");
+            Console.WriteLine($"\n2. feladat: A vetélkedőn {tesztek.Count} versenyző indult.");
+
+            Console.Write("\n3. feladat: A versenyző azonosítója = ");
+            string azon = Console.ReadLine().ToUpper();
+            Console.WriteLine("\t" + tesztek.Find(a => a.VersenyzoKod.Equals(azon)).Valaszok + "\t(a versenyző válasza)");
+
+            Console.WriteLine("\n4. feladat");
+            Console.WriteLine($"\t{joValasz}\t(a helyes megoldás)");
+            Console.WriteLine($"\t{tesztek.Find(a => a.VersenyzoKod.Equals(azon)).joValaszokString()}\t(a versenyző helyes válaszai)");
+
+            Console.Write("\n5. feladat: A feladat sorszáma = ");
+            int ssz;
+            do
+            {
+            } while (!int.TryParse(Console.ReadLine(), out ssz)||ssz<1||ssz>14);
+            int jok = tesztek.FindAll(a => a.kerdesJo(ssz-1)).Count();
+            Console.WriteLine($"\tA feladatra {jok} fő, a versenyzők {(100.0*jok/tesztek.Count).ToString("N2")}%-a adott helyes választ.");
+
+            Console.WriteLine("\n6. feladat: A versenyzők pontszámának meghatározása");
+
+            Console.WriteLine("\n7. feladat: A verseny legjobbjai:");
+            var topHaromPont = tesztek.Select(a => new  { pont = a.kapottPont }).OrderByDescending(b => b.pont).Distinct().Take(3).ToArray();
+            int helyezes = 0;
+            foreach (var item in topHaromPont)
+            {
+                helyezes++;
+                foreach (Teszt dijazott in tesztek.FindAll(a => a.kapottPont==item.pont))
+                {
+                    Console.WriteLine($"\t{helyezes}. díj ({dijazott.kapottPont} pont): {dijazott.VersenyzoKod}");
+                }
+            }
+            Console.WriteLine("\nProgram vége!");
+            Console.ReadKey();
+        }
+        static void Beolvas(string fajl)
+        {
+            using (StreamReader sr = new StreamReader(fajl))
+            {
+                joValasz = sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    tesztek.Add(new Teszt(sr.ReadLine().Split(' '), joValasz));
+                }
+            }
+        }
+    }
+}
---
+++ Tesztverseny/Teszt.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tesztverseny
+{
+    class Teszt
+    {
+        string versenyzoKod = null;
+        string valaszok = null;
+        public int kapottPont = 0;
+        char[] jok = new char[14];
+
+        public string VersenyzoKod { get => versenyzoKod; set => versenyzoKod = value; }
+        public string Valaszok { get => valaszok; set => valaszok = value; }
+
+        public Teszt(string[] sor, string joValaszok)
+        {
+            this.VersenyzoKod = sor[0];
+            this.Valaszok = sor[1];
+            for (int i = 0; i < this.Valaszok.Length; i++)
+            {
+                if (this.Valaszok[i].Equals(joValaszok[i]))
+                {
+                    jok[i] = '+';
+                    kapottPont += pontoz(i + 1);
+                }
+                else
+                {
+                    jok[i] = ' ';
+                }
+            }
+        }
+
+        public string joValaszokString()
+        {
+            return new string(jok);
+        }
+
+        int pontoz(int ssz)
+        {
+            int p = 0;
+            switch (ssz)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    p = 3;
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                    p = 4;
+                    break;
+                case 11:
+                case 12:
+                case 13:
+                    p = 5;
+                    break;
+                case 14:
+                    p = 6;
+                    break;
+                default:
+                    p = 0;
+                    break;
+            }
+            return p;
+        }
+        public bool kerdesJo(int ssz)
+        {
+            return jok[ssz].Equals('+');
+        }
+    }
+}
